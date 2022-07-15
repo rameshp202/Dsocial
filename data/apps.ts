ef1541418c9eadf1c39577782bc2e1801f1e936a
rameshp202 <109356441+rameshp202@users.noreply.github.com>
2022-07-15 19:04:37 +0545

    Add files via upload

---
+++ data/apps.ts
@@ -0,0 +1,32 @@
+export const apps = [
+  {
+    id: 'lenster',
+    name: 'Lenster',
+    logo: 'lenster.svg'
+  },
+  {
+    id: 'phaver',
+    name: 'Phaver',
+    logo: 'phaver.jpeg'
+  },
+  {
+    id: 'refract',
+    name: 'Refract',
+    logo: 'refract.jpeg'
+  },
+  {
+    id: 'iris',
+    name: 'iris',
+    logo: 'iris.png'
+  },
+  {
+    id: 'teaparty',
+    name: 'TeaParty',
+    logo: 'teaparty.png'
+  },
+  {
+    id: 'lenstube',
+    name: 'Lenstube',
+    logo: 'lenstube.svg'
+  }
+]
---
+++ src/lib/uploadAssetsToIPFS.ts
@@ -0,0 +1,29 @@
+import { LensterAttachment } from '@generated/lenstertypes'
+import axios from 'axios'
+
+const uploadAssetsToIPFS = async (data: any): Promise<LensterAttachment[]> => {
+  try {
+    const attachments = []
+    for (let i = 0; i < data.length; i++) {
+      let file = data.item(i)
+      const formData = new FormData()
+      formData.append('file', file, 'img')
+      const upload = await axios('https://ipfs.infura.io:5001/api/v0/add', {
+        method: 'POST',
+        data: formData
+      })
+      const { Hash }: { Hash: string } = upload?.data
+      attachments.push({
+        item: `https://ipfs.infura.io/ipfs/${Hash}`,
+        type: file.type,
+        altTag: ''
+      })
+    }
+
+    return attachments
+  } catch {
+    return []
+  }
+}
+
+export default uploadAssetsToIPFS
